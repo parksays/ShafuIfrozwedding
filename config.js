64f514d301ba53483c9900a9eee36f94d67bf312
parksays <uschannelpark@gmail.com>
2026-09-13 23:38:00 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,42 @@
+/*
+  ✏️ EDIT THIS FILE ONLY to change the invitation.
+  Names, dates, parents, venues, address, map links and family names are all here.
+*/
+const WEDDING = {
+  bride: {
+    name: "Shafiya",
+    parents: "Mrs. & Mr. Bride's Parents",
+    extra: "Add education / family details here"
+  },
+  groom: {
+    name: "Ifroz",
+    parents: "Mrs. & Mr. Groom's Parents",
+    extra: "Add education / family details here"
+  },
+
+  nikah: {
+    date: "6th October 2026",
+    iso: "2026-10-06T13:30:00+05:30",
+    time: "1:30 PM",
+    venue: "A.R. Function Hall",
+    address: "Near Mockuppal Railway Gate, MPL Road, Kadiri.",
+    map: "https://www.google.com/maps/search/?api=1&query=A.R.+Function+Hall+Kadiri"
+  },
+
+  valima: {
+    date: "6th October 2026",
+    iso: "2026-10-06T16:00:00+05:30",
+    time: "4:00 PM",
+    venue: "A.R. Function Hall",
+    address: "Near Mockuppal Railway Gate, MPL Road, Kadiri.",
+    map: "https://www.google.com/maps/search/?api=1&query=A.R.+Function+Hall+Kadiri"
+  },
+
+  family: [
+    "P. Saabjan Khan",
+    "N. Kareem Khan",
+    "N. Ahsanulla Khan",
+    "P. Rayyan Khan",
+    "Near & Dear"
+  ]
+};
